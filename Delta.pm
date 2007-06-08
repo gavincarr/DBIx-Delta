4360b5fc5959bfec6fda2a29f9486582439bbcdd
Gavin Carr <gavin@nox.openfusion.com.au>
2007-06-08 21:54:25 +1000

    Perldoc updates.

--- Delta.pm
+++ Delta.pm
@@ -216,24 +216,40 @@ containing arbitrary sql statements) to a database instance.
     }
     1;
 
-    # And then ...
-    perl -MFoo::Delta -le 'Foo::Delta->run'
+    # Then in a delta run script (e.g. delta.pl):
+    use Foo::Delta;
+    Foo::Delta->run;
+
+    # And requires a 'delta' table to track changes (see below)
+
+    # Then to check for deltas that have not been applied
+    ./delta.pl 
+    # And to apply those deltas and update the database
+    ./delta.pl -u
 
 
 =head1 DESCRIPTION
 
 DBIx::Delta is a module used to apply database deltas (changes) to a 
-database instance. Deltas are simply files containing arbitrary sql 
-statements, containing by a unique per-file tag. DBIx::Delta tracks which
-delta files have been applied by means of a special 'delta' table, and 
-will apply (execute) any deltas that are outstanding against your 
-database.
+database instance. It is intended for use in maintaining multiple 
+database schemas in sync e.g. you create deltas on your development 
+database instance, and subsequently apply those deltas to your test
+instance, and then finally to production. 
+
+It is simple and only requires DBI/DBD for your database connectivity.
+
+=head2 DELTAS
+
+Deltas are files containing arbitrary sql statements, identified by a 
+unique per-file tag. DBIx::Delta tracks which delta files have been 
+applied by means of a special 'delta' table, and will apply (execute) 
+any deltas that are outstanding against your database.
 
 DBIx::Delta files contain series of SQL statements to apply in the 
 database, and must also contain two additional metadata fields within 
 SQL comments, having the form 'key: value'. The first is a unique 
 identifier to be used as the delta_id, whose key may be anything ending
-in 'tag' e.g. 'tag', 'arch-tag'; the second is a description of the 
+in 'tag' e.g. 'tag', 'delta-tag' etc.; the second is a description of the 
 tables affected by this delta, whose key should be 'table' or 'tables'
 'tables' e.g.
 
@@ -243,33 +259,54 @@ tables affected by this delta, whose key should be 'table' or 'tables'
 or:
 
     -- tables: emp, emp_address
-    -- arch-tag: 7ae84801-e323-4a6f-984e-6de4f939a12c
-
+    -- delta-tag: 7ae84801-e323-4a6f-984e-6de4f939a12c
 
 DBIx::Delta tracks uses the tag identifier to track which delta files 
 have been applied by inserting a record into a special 'delta' table 
-that must exist in the database, with the following structure:
+that must exist in the database, with the following structure (SQLite):
 
     -- Create/bootstrap delta table (SQLite)
     create table delta (
-	delta_id 	varchar primary key,
-	delta_tables	varchar not null,
-	delta_apply_ts	timestamp default CURRENT_TIMESTAMP
+      delta_id 	        varchar primary key,
+      delta_tables	    varchar not null,
+      delta_apply_ts	timestamp default CURRENT_TIMESTAMP
     );
+
+and similarly, for mysql or postgresql:
+
+    -- Create/bootstrap delta table (mysql/postgresql)
+    create table delta (
+        delta_id        varchar(100) primary key,
+        delta_tables    varchar(255) not null,
+        delta_apply_ts  timestamp default now()
+    );
+
+
+=head2 USAGE
+
+    # Must be used via a subclass providing a db connection e.g.
+    package Foo::Delta;
+    use DBI;
+    use base qw(DBIx::Delta);
+    sub connect { 
+        DBI->connect('dbi:SQLite:dbname=foo.db','','');
+    }
+    1;
+
+    # And then ...
+    perl -MFoo::Delta -le 'Foo::Delta->run'
+
 
 =head1 AUTHOR
 
 Gavin Carr <gavin@openfusion.com.au>
 
-
 =head1 COPYRIGHT
 
-Copyright 2005-2006, Open Fusion Pty. Ltd. All Rights Reserved.
+Copyright 2005-2007, Open Fusion Pty. Ltd. All Rights Reserved.
 
 This program is free software. You may copy or redistribute it under the 
 same terms as perl itself.
 
 =cut
-
-# arch-tag: a41f466d-7930-4d6e-89cf-a9e6f698eebd
 
